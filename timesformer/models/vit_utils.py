f22862b89b0c67fa4313d81892e73e681079a2e5
William Chen <wchen6255@knights.ucf.edu>
2021-06-16 17:29:18 -0400

    compile errors

--- timesformer/models/vit_utils.py
+++ timesformer/models/vit_utils.py
@@ -11,7 +11,7 @@ import torch.nn.functional as F
 from timesformer.models.helpers import load_pretrained
 from .build import MODEL_REGISTRY
 from itertools import repeat
-from torch._six import container_abcs
+import collections.abc
 
 DEFAULT_CROP_PCT = 0.875
 IMAGENET_DEFAULT_MEAN = (0.485, 0.456, 0.406)
@@ -77,7 +77,7 @@ def trunc_normal_(tensor, mean=0., std=1., a=-2., b=2.):
 # From PyTorch internals
 def _ntuple(n):
     def parse(x):
-        if isinstance(x, container_abcs.Iterable):
+        if isinstance(x, collections.abc.Iterable):
             return x
         return tuple(repeat(x, n))
     return parse
